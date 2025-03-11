046b72bde57378806e50f11ee430eb02761f6852
Thanos Davaris <davthn08@gmail.com>
2025-03-11 21:25:41 +0200

    Added documentation to these files

--- Gym.py
+++ Gym.py
@@ -1,12 +1,14 @@
 import random
 import datetime
 
-dn = ['morning', 'afternoon']
-timeDay = [9,10,11,12]
-timeAfternoon = [2,3,4,5,6,7,8,9,10]
-timeSaturday = [2,3,4,5,6,7,8]
-today = datetime.datetime.today()
+#Initialising the variables
+dn = ['morning', 'afternoon'] #Time zones that the gym is open
+timeDay = [9,10,11,12] #Morning working hours of the gym
+timeAfternoon = [2,3,4,5,6,7,8,9,10] #Afternoon working hours of the gym
+timeSaturday = [2,3,4,5,6,7,8] #Special saturday hours
+today = datetime.datetime.today() #Variable that stores the date today
 
+#Condition that checks if the day is friday (The program is meant to be used the day before)
 if today.weekday() == 4:
   print("Tomorrow is Saturday so go at " + str(random.choice(timeSaturday)))
 else:
--- number guessing game.py
+++ number guessing game.py
@@ -1,19 +1,31 @@
 import random
 
+#Welcome message
 print("Welcome to my guessing game (by Prince of Aces)")
 
+#Initialising the variables and generating the random number
 lowest_num = 1
 highest_num = 10
 ran_num = random.randint(lowest_num, highest_num)
 lives = 3
+
+#Game start!
 while lives > 0:
+
+  #Guess input
   print(f"You have {lives} lives remaining")
   guess = int(input(f"guess a number between {lowest_num} and {highest_num}: "))
+
+  #Condition that checks if the guess is right
   if guess == ran_num:
+    #Win message
     print("Congrats! You've Won")
     exit()
   else:
+    #Failed try message
     print("Fail...")
   lives -= 1
+
+#Loss message
 print("You've ran out of lives")
 print(f"The number was {ran_num}")
