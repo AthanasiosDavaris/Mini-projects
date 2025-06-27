b3c4b25c2277783698ce5264c0650ecdf28710bc
Thanos Davaris <davthn08@gmail.com>
2025-06-27 20:35:54 +0300

    finished excercise 1

---
+++ Assembly/Ergasia_2_a.asm
@@ -0,0 +1,31 @@
+TITLE Expression Calculator
+
+INCLUDE Irvine32.inc
+
+p EQU 8d;
+
+.data
+x SDWORD 16d
+y SDWORD 154d
+z SDWORD -17d
+w SDWORD ?
+
+main PROC
+  mov eax, x
+  add eax, y
+
+  mov ebx, 50
+  add ebx, x
+  add ebx, y
+  add ebx, z
+  sub ebx, p
+
+  sub eax, ebx
+  mov w, eax
+
+  call WriteInt
+  call Crlf
+
+  exit
+main ENDP
+END main
